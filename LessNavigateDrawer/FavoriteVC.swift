cf8ed1564dc3714d3c4cc852074c0d588d5ba6ba
D261 <D261@D261s-MacBook-Pro.local>
2017-07-25 16:18:02 +0700

    first commit

---
+++ LessNavigateDrawer/FavoriteVC.swift
@@ -0,0 +1,22 @@
+//
+//  FavoriteVC.swift
+//  LessNavigateDrawer
+//
+//  Created by D261 on 7/25/17.
+//  Copyright © 2017 akak. All rights reserved.
+//
+
+import UIKit
+
+class FavoriteVC: BaseViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        addSlideMenuButton()
+        // Do any additional setup after loading the view.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+}
